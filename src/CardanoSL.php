e8659d84f79bb7ae169c7c4c70c3dd6de03368be
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-01-31 00:04:44 +0500

    Cardano-SL

--- src/CardanoSL.php
+++ src/CardanoSL.php
@@ -3,3 +3,44 @@ declare(strict_types=1);
 
 namespace CardanoSL;
 
+use CardanoSL\Http\AbstractHttpClient;
+use CardanoSL\Http\CardanoHttpAPI;
+
+/**
+ * Class CardanoSL
+ * @package CardanoSL
+ */
+class CardanoSL
+{
+    /** @var AbstractHttpClient */
+    private $httpClient;
+    /** @var string */
+    private $host;
+    /** @var int */
+    private $port;
+
+    /**
+     * CardanoSL constructor.
+     * @param string $host
+     * @param int $port
+     * @param AbstractHttpClient|null $httpClient
+     */
+    public function __construct(string $host, int $port, ?AbstractHttpClient $httpClient = null)
+    {
+        if (!$httpClient) {
+            $httpClient = new CardanoHttpAPI($host, $port);
+        }
+
+        $this->host = $host;
+        $this->port = $port;
+        $this->httpClient = $httpClient;
+    }
+
+    /**
+     * @return AbstractHttpClient
+     */
+    public function http(): AbstractHttpClient
+    {
+        return $this->httpClient;
+    }
+}
