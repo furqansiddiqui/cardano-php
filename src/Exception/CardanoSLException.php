23408b0a0e9791313f70cd7a6b1c454672db2f40
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-01-30 23:36:11 +0500

    Groundbreaking

---
+++ src/Exception/CardanoSLException.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace CardanoSL\Exception;
+
+/**
+ * Class CardanoSLException
+ * @package CardanoSL\Exception
+ */
+class CardanoSLException extends \Exception
+{
+}
