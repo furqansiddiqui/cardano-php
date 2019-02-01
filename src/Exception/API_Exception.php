6f1d8f10e1d195fe88044f1d545faca005d3a483
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-02-01 19:52:15 +0500

    Error Handling

---
+++ src/Exception/API_Exception.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace CardanoSL\Exception;
+
+/**
+ * Class API_Exception
+ * @package CardanoSL\Exception
+ */
+class API_Exception extends CardanoSLException
+{
+}
--- src/Http/CardanoHttpAPI.php
+++ src/Http/CardanoHttpAPI.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace CardanoSL\Http;
 
+use CardanoSL\Exception\API_Exception;
 use CardanoSL\Exception\HttpAPIException;
 use HttpClient\Request;
 use HttpClient\Response\JSONResponse;
@@ -19,6 +20,7 @@ class CardanoHttpAPI extends AbstractHttpClient
      * @param string $endpoint
      * @param array|null $payload
      * @return HttpJSONResponse
+     * @throws API_Exception
      * @throws HttpAPIException
      * @throws \HttpClient\Exception\HttpClientException
      * @throws \HttpClient\Exception\RequestException
@@ -48,10 +50,28 @@ class CardanoHttpAPI extends AbstractHttpClient
         }
 
         if ($res instanceof JSONResponse) {
+            $payload = $res->array();
+            $status = $payload["status"] ?? null;
+            $httpCodeStatus = $res->code() >= 200 && $res->code() < 300 ? true : false;
+
+            if ($status !== "success" || !$httpCodeStatus) {
+                $msg = $payload["message"] ?? null;
+                $detailMsg = $payload["diagnostic"]["msg"] ?? $payload["diagnostic"]["details"]["msg"] ?? null;
+
+                if ($msg && $detailMsg) {
+                    throw new API_Exception(sprintf('[%s]: %s', $msg, $detailMsg), $res->code());
+                } elseif ($msg) {
+                    throw new API_Exception(sprintf('Cardano SL API error: %s', $msg), $res->code());
+                } else {
+                    throw new API_Exception('Cardano SL API call not successful, unknown error', $res->code());
+                }
+            }
+
             $jsonResponse = new HttpJSONResponse();
             $jsonResponse->httpCode = $res->code();
-            $jsonResponse->payload = $res->array();
+            $jsonResponse->payload = $payload;
             $jsonResponse->headers = $res->headers();
+
             return $jsonResponse;
         }
 
