a967cddbd0939aec20264e46025fec8657f48b7b
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-02-01 23:42:47 +0500

    more validations

--- src/Validate.php
+++ src/Validate.php
@@ -10,6 +10,46 @@ namespace CardanoSL;
 class Validate
 {
     /**
+     * @param $id
+     * @return bool
+     */
+    public static function WalletIdentifier($id): bool
+    {
+        if (is_string($id) && preg_match('/^[a-zA-Z0-9]{8,128}$/', $id)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param $name
+     * @return bool
+     */
+    public static function WalletName($name): bool
+    {
+        return is_string($name) && preg_match('/^[\w\s\.\-]{3,32}$/', $name) ? true : false;
+    }
+
+    /**
+     * @param $level
+     * @return bool
+     */
+    public static function AssuranceLevel($level): bool
+    {
+        return is_string($level) && in_array($level, ["normal", "strict"]) ? true : false;
+    }
+
+    /**
+     * @param $tag
+     * @return bool
+     */
+    public static function SyncStateTag($tag): bool
+    {
+        return is_string($tag) && in_array($tag, ["restoring", "synced"]) ? true : false;
+    }
+
+    /**
      * @param $hash
      * @return bool
      */
