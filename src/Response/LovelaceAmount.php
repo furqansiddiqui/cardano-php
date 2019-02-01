6c55dc569fafe8ff501541191828b62d3fb60567
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-02-01 22:30:04 +0500

    Lovelace amount block

---
+++ src/Response/LovelaceAmount.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace CardanoSL\Response;
+
+use CardanoSL\CardanoSL;
+use CardanoSL\Exception\API_ResponseException;
+
+/**
+ * Class LovelaceAmount
+ * @package CardanoSL\Response
+ */
+class LovelaceAmount implements ResponseModelInterface
+{
+    /** @var int */
+    public $lovelace;
+    /** @var string */
+    public $ada;
+
+    /**
+     * LovelaceAmount constructor.
+     * @param null $lovelaceAmount
+     * @param string|null $which
+     * @throws API_ResponseException
+     */
+    public function __construct($lovelaceAmount = null, ?string $which = null)
+    {
+        if ($which) {
+            $which = sprintf('"%s" ', $which);
+        }
+
+        if (!is_int($lovelaceAmount)) {
+            throw new API_ResponseException($which . 'Lovelace amount must be an integer');
+        } elseif ($lovelaceAmount > CardanoSL::MAX_LOVELACE) {
+            throw new API_ResponseException($which . sprintf('Lovelace amount cannot exceed %d', CardanoSL::MAX_LOVELACE));
+        }
+
+        $this->lovelace = $lovelaceAmount;
+        $this->ada = bcsub(strval($this->lovelace), bcpow("10", "6", 0), 6);
+    }
+}
