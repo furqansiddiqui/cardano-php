09a3e083f31c48118a163438dc25d59e192f7e5d
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-02-01 23:42:21 +0500

    Wallet Info objects

--- src/Response/WalletInfo.php
+++ src/Response/WalletInfo.php
@@ -3,6 +3,10 @@ declare(strict_types=1);
 
 namespace CardanoSL\Response;
 
+use CardanoSL\Exception\API_ResponseException;
+use CardanoSL\Http\HttpJSONResponse;
+use CardanoSL\Validate;
+
 /**
  * Class WalletInfo
  * @package CardanoSL\Response
@@ -27,4 +31,42 @@ class WalletInfo implements ResponseModelInterface
     public $syncState;
     /** @var string */
     public $type;
+
+    /**
+     * WalletInfo constructor.
+     * @param HttpJSONResponse $res
+     * @throws API_ResponseException
+     */
+    public function __construct(HttpJSONResponse $res)
+    {
+        $data = $res->payload["data"] ?? null;
+        if (!is_array($data) || !$data) {
+            throw API_ResponseException::RequirePropMissing("data");
+        }
+
+        $this->id = Validate::WalletIdentifier($data["id"] ?? null) ? $data["id"] : null;
+        if (!$this->id) {
+            throw API_ResponseException::InvalidPropValue("id");
+        }
+
+        $this->assuranceLevel = $data["assuranceLevel"] ?? null;
+        if (!Validate::AssuranceLevel($this->assuranceLevel)) {
+            throw API_ResponseException::InvalidPropValue("wallet.assuranceLevel");
+        }
+
+        $this->balance = new LovelaceAmount($data["balance"] ?? null, "wallet.Balance");
+        $this->createdAt = $data["createdAt"] ?? null;
+        $this->hasSpendingPassword = array_key_exists("hasSpendingPassword", $data) && is_bool($data["hasSpendingPassword"]) ?
+            $data["hasSpendingPassword"] : null;
+        $this->name = $data["name"] ?? null;
+        $this->spendingPasswordLastUpdate = $data["spendingPasswordLastUpdate"] ?? null;
+        $this->type = $data["type"] ?? null;
+
+        $syncState = $data["syncState"] ?? null;
+        if (!is_array($syncState) || !$syncState) {
+            throw API_ResponseException::RequirePropMissing("wallet.syncState");
+        }
+
+        $this->syncState = WalletSyncState::Construct($syncState);
+    }
 }
--- src/Response/WalletSyncState.php
+++ src/Response/WalletSyncState.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace CardanoSL\Response;
 
+use CardanoSL\Exception\API_ResponseException;
+use CardanoSL\Validate;
+
 /**
  * Class WalletSyncState
  * @package CardanoSL\Response
@@ -11,10 +14,44 @@ class WalletSyncState implements ResponseModelInterface
 {
     /** @var string */
     public $tag;
-    /** @var QuantityUnitBlock */
+    /** @var null|QuantityUnitBlock */
     public $estimatedCompletionTime;
-    /** @var QuantityUnitBlock */
+    /** @var null|QuantityUnitBlock */
     public $percentage;
-    /** @var QuantityUnitBlock */
+    /** @var null|QuantityUnitBlock */
     public $throughput;
+
+    /**
+     * @param array $block
+     * @return WalletSyncState
+     * @throws API_ResponseException
+     */
+    public static function Construct(array $block): self
+    {
+        $syncState = new self();
+        $syncState->tag = $block["tag"] ?? null;
+        if (!Validate::SyncStateTag($syncState->tag)) {
+            throw API_ResponseException::InvalidPropValue("wallet.syncState.Tag");
+        }
+
+        $syncStateData = $block["data"] ?? null;
+        if ($syncState->tag === "restoring" || is_array($syncStateData)) {
+            $syncState->estimatedCompletionTime = QuantityUnitBlock::Construct(
+                "wallet.syncState.data.estimatedCompletionTime",
+                $syncStateData["estimatedCompletionTime"] ?? null
+            );
+
+            $syncState->percentage = QuantityUnitBlock::Construct(
+                "wallet.syncState.data.percentage",
+                $syncStateData["percentage"] ?? null
+            );
+
+            $syncState->throughput = QuantityUnitBlock::Construct(
+                "wallet.syncState.data.throughput",
+                $syncStateData["throughput"] ?? null
+            );
+        }
+
+        return $syncState;
+    }
 }
