828a1a7575166f1a79272c0064174bc6c613bd83
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-02-01 23:47:42 +0500

    return Mnemonic for created/restored Wallet instances

--- src/API/Wallet.php
+++ src/API/Wallet.php
@@ -7,6 +7,7 @@ use CardanoSL\CardanoSL;
 use CardanoSL\Exception\WalletException;
 use CardanoSL\Response\WalletInfo;
 use CardanoSL\Validate;
+use furqansiddiqui\BIP39\Mnemonic;
 
 /**
  * Class Wallet
@@ -20,15 +21,18 @@ class Wallet
     private $id;
     /** @var null|WalletInfo */
     private $info;
+    /** @var null|Mnemonic */
+    private $mnemonic;
 
     /**
      * Wallet constructor.
      * @param CardanoSL $node
      * @param string $id
      * @param WalletInfo|null $walletInfo
+     * @param Mnemonic|null $mnemonic
      * @throws WalletException
      */
-    public function __construct(CardanoSL $node, string $id, ?WalletInfo $walletInfo = null)
+    public function __construct(CardanoSL $node, string $id, ?WalletInfo $walletInfo = null, ?Mnemonic $mnemonic = null)
     {
         $this->node = $node;
         $this->id = $id;
@@ -36,6 +40,7 @@ class Wallet
         if ($walletInfo) {
             $this->id = $walletInfo->id;
             $this->info = $walletInfo;
+            $this->mnemonic = $mnemonic;
         }
 
         self::isValidIdentifier($this->id);
@@ -63,6 +68,15 @@ class Wallet
         // Get wallet info
         $walletInfo = $this->node->http()->get(sprintf('/api/v1/wallets/%s', $this->id));
         return new WalletInfo($walletInfo);
+    }
+
+    /**
+     * ONLY return Mnemonic object for instances result of create/restore operations
+     * @return null|Mnemonic
+     */
+    public function mnemonic(): ?Mnemonic
+    {
+        return $this->mnemonic;
     }
 
     /**
