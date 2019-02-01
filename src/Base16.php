b0c05042f4eda299f92256801fd75be127236c4a
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-02-01 20:54:17 +0500

    Hexadecimal encoder

---
+++ src/Base16.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace CardanoSL;
+
+/**
+ * Class Base16
+ * @package CardanoSL
+ */
+class Base16
+{
+    /**
+     * @param string $str
+     * @return string
+     */
+    public static function Encode(string $str): string
+    {
+        $hex = "";
+        for ($i = 0; $i < strlen($str); $i++) {
+            $hex .= dechex(ord($str[$i]));
+        }
+
+        return $hex;
+    }
+
+    /**
+     * @param string $hex
+     * @return string
+     */
+    public static function Decode(string $hex): string
+    {
+        $str = "";
+        for ($i = 0; $i < strlen($hex) - 1; $i += 2) {
+            $str .= chr(hexdec($hex[$i] . $hex[$i + 1]));
+        }
+
+        return $str;
+    }
+}
