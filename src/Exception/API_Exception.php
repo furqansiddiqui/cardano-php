7ce38076da37d223e8dce8fd329f81bfa2b35d24
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-02-02 16:40:40 +0500

    API Exceptions

--- src/Exception/API_Exception.php
+++ src/Exception/API_Exception.php
@@ -9,4 +9,22 @@ namespace CardanoSL\Exception;
  */
 class API_Exception extends CardanoSLException
 {
+    /**
+     * @param string $which
+     * @param string|null $expected
+     * @param string|null $got
+     * @return API_Exception
+     */
+    public static function InvalidPropValue(string $which, ?string $expected = null, ?string $got = null): self
+    {
+        $message = sprintf('Invalid value for prop. "%s"', $which);
+        if ($expected) {
+            $message .= sprintf('; Expected "%s"', $expected);
+            if ($got) {
+                $message .= sprintf(' got "%s"', $got);
+            }
+        }
+
+        return new self($message);
+    }
 }
--- src/Exception/API_ResponseException.php
+++ src/Exception/API_ResponseException.php
@@ -17,23 +17,4 @@ class API_ResponseException extends API_Exception
     {
         return new self(sprintf('Required prop. "%s" not found in API response', $which));
     }
-
-    /**
-     * @param string $which
-     * @param string|null $expected
-     * @param string|null $got
-     * @return API_ResponseException
-     */
-    public static function InvalidPropValue(string $which, ?string $expected = null, ?string $got = null): self
-    {
-        $message = sprintf('Invalid value for prop. "%s"', $which);
-        if ($expected) {
-            $message .= sprintf('; Expected "%s"', $expected);
-            if ($got) {
-                $message .= sprintf(' got "%s"', $got);
-            }
-        }
-
-        return new self($message);
-    }
 }
