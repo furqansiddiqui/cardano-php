7613889bedbf56b07fdd7b4ee0de252aeb1c36aa
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-06-27 15:47:16 +0500

    update

--- src/API/RawTransaction.php
+++ src/API/RawTransaction.php
@@ -41,7 +41,7 @@ class RawTransaction
 
         $this->payees[] = [
             "address" => $address,
-            "amount" => $amount->ada
+            "amount" => $amount->lovelace
         ];
 
         return $this;
